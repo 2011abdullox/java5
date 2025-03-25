305194e10aba7106024134e11de8ab97ea0442cd
2011abdullox <abdullox20090831@gmail.com>
2025-03-25 11:37:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let name1 = prompt(" Ismingizi kiritin");
+console.log("name :", name1);
+
+let name2 = prompt("Mars idni kiritin");
+console.log(name2);
+
+let name3 = prompt("mars parolini kiritin");
+console.log(name3); 
+
+let obj1 = {name1: 'jorabek',  name2: '1147506', name3:'29386'};
